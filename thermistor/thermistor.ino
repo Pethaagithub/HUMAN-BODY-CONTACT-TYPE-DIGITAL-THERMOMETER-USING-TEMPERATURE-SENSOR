56102c5e5acb5f25f1a0dfdd44c6defc564c0aee
Pethaagithub <140972149+Pethaagithub@users.noreply.github.com>
2023-07-31 06:32:34 +0530

    Add files via upload

---
+++ thermistor/thermistor.ino
@@ -0,0 +1,100 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include<stdio.h>
+#include<math.h>
+#include "index.h"  //Web page header file
+
+//--------------------------------- DEFINITIONS ---------------------------------
+
+const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
+const double R2 = 10000;            // 10k ohm series resistor
+const double adc_resolution = 1023; // 10-bit adc
+
+const double A = 0.001129148;   // thermistor equation parameters
+const double B = 0.000234125;
+const double C = 0.0000000876741; 
+
+#define TEMP_PIN 35
+WebServer server(80);
+
+//Enter your SSID and PASSWORD
+const char* ssid = "SASTRA";
+const char* password = "124006052";
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+
+void handleRoot() 
+{
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+ 
+void handleADC() 
+{
+  double Vout, Rth, temperature, adc_value; 
+  adc_value = analogRead(TEMP_PIN);   //analog pin in ESP32
+  Vout = (adc_value * VCC) / adc_resolution;
+  Rth = (VCC * R2 / Vout) - R2;
+
+  //Steinhart-Hart Thermistor Equation:
+  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   // Temperature in kelvin
+  temperature = temperature - 273.15;  // Temperature in degree celsius
+  String adcValue = String(temperature);
+  
+  server.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
+}
+
+//===============================================================
+// Setup
+//===============================================================
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+
+/*
+//ESP32 As access point
+  WiFi.mode(WIFI_AP); //Access Point mode
+  WiFi.softAP(ssid, password);
+*/
+//ESP32 connects to your wifi -----------------------------------
+  WiFi.mode(WIFI_STA); //Connectto your wifi
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
+
+  //Wait for WiFi to connect
+  while(WiFi.waitForConnectResult() != WL_CONNECTED)
+    Serial.print(".");
+    
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+
+//----------------------------------------------------------------
+ 
+  server.on("/", handleRoot);      //This is display page
+  server.on("/readADC", handleADC);//To get update of ADC Value only
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+
+void loop(void)
+{
+  server.handleClient();
+  delay(1);
+}
